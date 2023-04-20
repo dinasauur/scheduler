8312ad7229dcd25b8786fdee5d35cc5561243b5f
Wendina <wendina.teng@gmail.com>
2023-04-20 02:05:03 -0400

    Modify the className functions because css wasn't applying correctly to the original written function.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,14 +5,17 @@ import './InterviewerListItem.scss'
 function InterviewerListItem(props) {
 
   const interviewerClass = classNames('interviewers__item', {
-    'interviewers__item-image': props.avatar,
     'interviewers__item--selected': props.selected
   });
+
+  const imageClass = classNames('interviewers__item-image', {
+    'interviewers__item--selected-image': props.selected
+  })
 
   return (
     <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
-        className={interviewerClass}
+        className={imageClass}
         src={props.avatar}
         alt={props.name}
       />
