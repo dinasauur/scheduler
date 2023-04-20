1660de56488005231fd6d202f4f88bbd17ba9bb0
Wendina <wendina.teng@gmail.com>
2023-04-20 01:57:25 -0400

    Build InterviewerListItem copmponenent. Replace hard-coded values with props, add an event handler to <li>, conditionally add classes, and conditionally render the interviewer's name.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,22 @@
 import React from "react";
+import classNames from "classnames";
+import './InterviewerListItem.scss'
 
 function InterviewerListItem(props) {
+
+  const interviewerClass = classNames('interviewers__item', {
+    'interviewers__item-image': props.avatar,
+    'interviewers__item--selected': props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        className={interviewerClass}
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected ? props.name : null}
     </li>
   );
 }
