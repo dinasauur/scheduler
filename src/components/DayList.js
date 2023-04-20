60de296efcc85085dd20d1eb9d07f194c49497bc
Wendina <wendina.teng@gmail.com>
2023-04-20 02:52:54 -0400

    Refactoring the dayList components to maintain consitency in technique even though the same number of props is being passed down.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,11 +3,21 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
+  const days = props.days.map(day => {
+    return (
+      <DayListItem
+        key={day.id}
+        spots={day.spots}
+        name={day.name}
+        selected={day.name === props.day}
+        setDay={() => props.setDay(props.name)}
+      />
+    );
+  });
+
   return (
     <ul>
-      {props.days.map(day => {
-    return <DayListItem key={day.id} spots={day.spots} name={day.name} selected={day.name === props.day} setDay={props.setDay}/>
-  })}
+      {days}
     </ul>
   );
 }
