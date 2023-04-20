f725b669fdee5c6f2f98559717742edc97758fb9
Wendina <wendina.teng@gmail.com>
2023-04-20 02:08:34 -0400

    Create new component for InterviewerList.

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+
+function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+
+      </ul>
+    </section>
+  );
+}
+
+export default InterviewerList;
