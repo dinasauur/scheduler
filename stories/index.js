9b081b2b0d53aa7b014378216933df1aaf9b6145
Wendina <wendina.teng@gmail.com>
2023-04-20 21:47:19 -0400

    Add stories for the Form component - edit and create form. The student and interviewer attributes are hard-coded for now.

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 // Button
 storiesOf("Button", module)
@@ -176,4 +177,20 @@ storiesOf("Appointment", module)
       message="Could not delete appointment."
       onClose={action("onClose")}
     />
-  ));
+  ))
+  .add("Edit-Form", () => (
+    <Form 
+      student="Dina"
+      interviewer="1"
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create-Form", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
