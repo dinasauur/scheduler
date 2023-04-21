423eee18e4c6e3881c1ca325710acaf9f04e445d
Wendina <wendina.teng@gmail.com>
2023-04-21 01:25:31 -0400

    Update the controlled list onChange, and use props onSave and onCancel in the appropriate places.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,7 +4,7 @@ import Button from "components/Button";
 
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
-  const [interviewer, setInterviewer] =useState(props.interviewer || null);
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -17,23 +17,19 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
-          /*
-          This must be a controlled component
-          your code goes here
-        */
           />
           <InterviewerList 
             interviewers={props.interviewers} 
             value={interviewer}
-            onChange={(event) => setInterviewer(event.target.value)}
+            onChange={setInterviewer}
           />
         </form>
       </section>
 
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger >Cancel</Button>
-          <Button confirm >Save</Button>
+          <Button onClick={props.onCancel} danger >Cancel</Button>
+          <Button onClick={props.onSave} confirm >Save</Button>
         </section>
       </section>
     </main>
