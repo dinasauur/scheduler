32f837b3e1978f6a524ade50d49acac58874cb9c
Wendina <wendina.teng@gmail.com>
2023-04-25 01:55:54 -0400

    Update the Appointment component with the appropriate logic to handle the transition to CREATE.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,29 +1,38 @@
-import React from "react";
-import "components/Appointment/styles.scss";
-import Header from "./Header";
-import Show from "./Show";
-import Empty from "./Empty";
-import useVisualMode from "hooks/useVisualMode";
+import React from 'react';
 
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
+import 'components/Appointment/styles.scss';
+
+import useVisualMode from 'hooks/useVisualMode';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+import Form from './Form';
+
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
 export default function Appointment(props) {
-
   const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY;
+    props.interview ? SHOW : EMPTY
   );
 
   return (
-    <article className='appointment'>
+    <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log('CLicked onAdd')} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-        <Show 
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
         />
       )}
+      {mode === CREATE &&
+        <Form
+        interviewers={[]}
+        onCancel={back}
+        onSave={console.log('This is the onSave button')} // Change this in the future. Make save function. 
+        />}
     </article>
   );
 }
