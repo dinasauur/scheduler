6b75a49e985f5ac71820a6d8991d71434c666b19
Wendina <wendina.teng@gmail.com>
2023-04-24 20:02:39 -0400

    Add transition property to custom hook to the object that it returns.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
 
+// STEP 1- take in an initial mode, set the mode state with the initial mode provided, return object with mode property
+// STEP 2- create a transition function within useVisualMode that will take in new mode and update the mode state with the new value. Use a custom hook.
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  
+  // STEP 2
+  function transition(mode) {
+    setMode(mode);
+  }
 
-  return { mode };
+  return { mode, transition };
 };
