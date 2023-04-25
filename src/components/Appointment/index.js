6da5ec7d1e1d91531057bdc0f07f0b5ef2cc8016
Wendina <wendina.teng@gmail.com>
2023-04-25 00:54:17 -0400

    Conditionally rendering the first components. Import useVisualMode into the Appointment Component. Render empty and show.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,20 +3,27 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY;
+  );
 
   return (
     <article className='appointment'>
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => console.log('CLicked onAdd')} />}
+      {mode === SHOW && (
         <Show 
         student={props.interview.student}
         interviewer={props.interview.interviewer}
         />
-        :
-        <Empty />
-      }
+      )}
     </article>
   );
 }
