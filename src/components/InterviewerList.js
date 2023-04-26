c06d646de01f81be87496ac401fc6ffd02572d81
Wendina <wendina.teng@gmail.com>
2023-04-26 15:33:16 -0400

    Add prop-type dependency. Import the object in InterviewList and define the validatioin configuratiion for that component.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import './InterviewerList.scss';
+import PropTypes from 'prop-types';
 
 function InterviewerList(props) {
 
@@ -25,5 +26,9 @@ function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default InterviewerList;
