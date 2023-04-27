4f98fe8d3c6a6d0e894a462805e8749c7ca41d4d
Wendina <wendina.teng@gmail.com>
2023-04-27 02:02:11 -0400

    creating a mocks folders with emptyy axios.js file. This file will export an obejct with the three functions that we use in the useApplicatioonData hook.

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,4 @@
+// It is important that the name of the module matches the name of the library that we are mocking
+// The file will export an object with the three functions that we use in the useApplicationData Hook.
+// The Jest framework will replace any axios import with this mock module instead.
+
