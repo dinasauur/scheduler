c6c1efbdb4f324dde80b8346045645320f1216cd
Wendina <wendina.teng@gmail.com>
2023-04-27 02:36:56 -0400

    testing with test data. the first test is to make request to API server to retrieve appointment data.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,36 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
-});
+describe('Application', () => {
+  // application loads without crashing and makes the requests to the API server to retrieve appointment data to display in the schedule
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    //  waitForElement is a function that returns a DOM node. In this case, it is looking for something based on the text "Monday"
+    return waitForElement(() => getByText('Monday'))
+    .then(() => {
+      fireEvent.click(getByText('Tuesday'));
+      expect(getByText('Leopold Silvers')).toBeInTheDocument();
+    })
+  });
+  
+})
+
+
+
+// write the tests to confirm that our Application component makes the necessary requests to the API. 
+// To accomplish this will need to replace the real implementation of the axios library with a mock version. 
+// We will use mocks to remove the dependency on HTTP request library specifically during testing.
+
+// We will mock the functions we use from the axios library. [DONE]
+// We will write a test to confirm that the scheduler can load data. [IN-PROGRESS]
+// We will write an asynchronous test that waits for a component to update before proceeding.
+// We will use containers to find specific DOM nodes.
+// We will chain promises to handle asynchronous testing.
+// We will override mock implementations for specific tests.
+// We will use setup and teardown functions provided by Jest to perform common tasks.
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,23 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 
-import Application from "components/Application";
+import Application from 'components/Application';
 
 describe('Appointment', () => {
   it('renders without crashing', () => {
     render(<Application />);
   });
-})
-
-// write the tests to confirm that our Application component makes the necessary requests to the API. 
-// To accomplish this will need to replace the real implementation of the axios library with a mock version. 
-// We will use mocks to remove the dependency on HTTP request library specifically during testing.
-
-// We will mock the functions we use from the axios library.
-// We will write a test to confirm that the scheduler can load data.
-// We will write an asynchronous test that waits for a component to update before proceeding.
-// We will use containers to find specific DOM nodes.
-// We will chain promises to handle asynchronous testing.
-// We will override mock implementations for specific tests.
-// We will use setup and teardown functions provided by Jest to perform common tasks.
+});
