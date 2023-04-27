206003a6f75a2f97f6134bb6749a9c806f9732d4
Wendina <wendina.teng@gmail.com>
2023-04-26 21:54:15 -0400

    Implement the third, fourth, and fifth Form test in preparation for adding the input field and interviewer selection validation feature.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -33,6 +33,7 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
           <InterviewerList 
             interviewers={props.interviewers} 
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -8,6 +8,8 @@ describe('Appointment', () => {
   it('renders without crashing', () => {
     render(<Application />);
   });
-
-
 })
+
+// write the tests to confirm that our Application component makes the necessary requests to the API. 
+// To accomplish this will need to replace the real implementation of the axios library with a mock version. 
+// We will use mocks to remove the dependency on HTTP request library specifically during testing.
