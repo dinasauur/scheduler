f792da24719371c02eaa93673a74c6590b45ce0b
Wendina <wendina.teng@gmail.com>
2023-04-27 22:12:12 -0400

    add test that it should cancel an interview.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -19,7 +19,7 @@ describe('appointments', () => {
     cy.contains('.appointment__card--show', 'Sylvia Palmer');
   });
 
-  it('should edit an interview', () => {
+  xit('should edit an interview', () => {
     cy.get('[alt=Edit]').first().click({ force: true });
 
     cy.get('[data-testid="student-name-input"')
@@ -31,5 +31,16 @@ describe('appointments', () => {
 
     cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
     cy.contains('.appointment__card--show', 'Tori Malcolm');
+  });
+
+  it('should cancel an interview', () => {
+    cy.get('[alt=Delete]').first().click({ force: true });
+
+    cy.contains('Confirm').click();
+
+    cy.contains('Deleting...').should('exist');
+    cy.contains('Deleting...').should('not.exist');
+
+    cy.contains('.appointment__card--show', 'Archie Cohen').should('not.exist');
   });
 });
